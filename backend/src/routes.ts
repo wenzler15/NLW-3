75cdaaafd9710794c263c71f3760ef67c5d4d41c
Douglas Wenzler <douglaswenzler@MacBook-Air-de-Douglas.local>
2020-10-15 17:36:24 -0300

    second Class

---
+++ backend/src/routes.ts
@@ -0,0 +1,13 @@
+import {Router} from 'express';
+import OrphanagesController from './controllers/OrphanagesController';
+import multer from 'multer';
+import uploadConfig from './config/upload';
+
+const routes = Router();
+const upload = multer();
+
+routes.post('/orphanage', upload.array('images'), OrphanagesController.create);
+routes.get('/orphanage', OrphanagesController.index);
+routes.get('/orphanage/:id', OrphanagesController.show);
+
+export default routes;
